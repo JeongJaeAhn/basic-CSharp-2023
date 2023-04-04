d0d331dc9119c438521440dd5316cd89485b35ba
JeongJaeAhn <123913903+JeongJaeAhn@users.noreply.github.com>
2023-04-04 12:13:17 +0900

    C# 첫번째 커밋

---
+++ Day01/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// 도구 - 설정 가져오기 및 내보내기
+// 네임스페이스 ConsoleApp1
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// 프로그램 클래스
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// 메인 엔트리 포인트
+        /// </summary>
+        /// <param name="args">콘솔 매개변수 </param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!!"); // Alt + Enter 전구 어시턴스 로잘리
+        }
+    }
+}
