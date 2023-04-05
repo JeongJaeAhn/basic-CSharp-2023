a7acd4d74f61f65b9e9de946d5a953e53554a784
JeongJaeAhn <123913903+JeongJaeAhn@users.noreply.github.com>
2023-04-05 17:17:42 +0900

    23.4.5 수

---
+++ Day02/Day02App/cs12_methods/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs12_methods
+{
+    class Calc
+    {
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+    internal class Program
+    {
+        /// <summary>
+        /// 실행시 메모리에 최초 올라가야 하기때문에 static이 되어야 하고
+        /// 메서드 이름이 Main 이면 실행될때 알아서 제일 처음에 시작된다.
+        /// </summary>
+        /// <param name="args"></param>
+        
+        static void Main(string[] args)
+        {
+            #region < static 메서드>
+            int result = Calc.Plus(1, 2);
+            // static은 최초실행때 메모리에 바로 올라가기때문에
+            // 클래스를 객체를 만들 필요가 없음 like new Calc();
+
+            // Calc.Minus(3, 2); // Minus는 static이 아니기때문에 접근불가 (객체생성해야 접근가능)
+            Console.WriteLine(result);
+            int result2 = Calc.Minus(2, 2);
+            Console.WriteLine(result2);
+            #endregion
+
+            #region < Call by reference vs Call by value 비교>
+            int x = 10; int y = 3;
+            Swap(ref x, ref y); // x, y가 가지고 있는 주소를 전달하라 Call by reference == pointer
+
+            Console.WriteLine("x = {0}, y = {1}", x, y);
+
+            Console.WriteLine(GetNumber());
+            #endregion
+
+            #region < out 매개변수 >
+
+            int divid = 30;
+            int divor = 2;
+
+            int rem = 0;
+            Divide(divid, divor, out result, out rem);
+            Console.WriteLine("나누기값 {0}, 나머지 {1}", result, rem);
+
+            (result, rem) = Divide(20, 6);
+            Console.WriteLine("나누기값 {0}, 나머지 {1}", result, rem);
+
+            #endregion
+
+            #region < 가변길이 매개변수 >
+
+            Console.WriteLine(Sum(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+            #endregion
+        }
+
+
+        ///static int Divide(int x, int y)
+        ///{
+        ///    return x / y;
+        //}
+        ///static int Reminder(int x, int y)
+        ///{
+        ///    return x % y;
+        ///}
+
+        static void Divide(int x, int y, out int val, out int rem)
+        {
+            val = x / y;
+            rem = x % y;
+        }
+
+        static (int result, int rem) Divide(int x, int y)
+        {
+            return (x / y, x % y);  // C# 7.0 이후로부터
+        }
+
+        // Main 메서드와 같은 레벨에 있는 메서드들은 전부 static이 되어야 함(무조건!)
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = 0;
+            temp = a;   // 5 : temp = 5
+            a = b;  // a = 4
+            b = temp; // 5
+        }
+
+        static int val = 100;
+
+        public static ref int GetNumber()
+        {
+            return ref val; // static 메서드에 접근할 수 있는 변수는 static밖에 없음
+        }
+        
+
+    }
+}
---
+++ Day02/Day02WinApp/wf03_property/FrmMain.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace wf03_property
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.GbxMain = new System.Windows.Forms.GroupBox();
+            this.NudFontSize = new System.Windows.Forms.NumericUpDown();
+            this.FntSize = new System.Windows.Forms.Label();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.Chkitalic = new System.Windows.Forms.CheckBox();
+            this.ChkBold = new System.Windows.Forms.CheckBox();
+            this.CboFontFamily = new System.Windows.Forms.ComboBox();
+            this.Fnt = new System.Windows.Forms.Label();
+            this.GbxMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NudFontSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GbxMain
+            // 
+            this.GbxMain.Controls.Add(this.NudFontSize);
+            this.GbxMain.Controls.Add(this.FntSize);
+            this.GbxMain.Controls.Add(this.TxtResult);
+            this.GbxMain.Controls.Add(this.Chkitalic);
+            this.GbxMain.Controls.Add(this.ChkBold);
+            this.GbxMain.Controls.Add(this.CboFontFamily);
+            this.GbxMain.Controls.Add(this.Fnt);
+            this.GbxMain.Location = new System.Drawing.Point(11, 12);
+            this.GbxMain.Name = "GbxMain";
+            this.GbxMain.Size = new System.Drawing.Size(448, 204);
+            this.GbxMain.TabIndex = 0;
+            this.GbxMain.TabStop = false;
+            this.GbxMain.Text = "groupBox1";
+            // 
+            // NudFontSize
+            // 
+            this.NudFontSize.Location = new System.Drawing.Point(86, 65);
+            this.NudFontSize.Name = "NudFontSize";
+            this.NudFontSize.Size = new System.Drawing.Size(120, 21);
+            this.NudFontSize.TabIndex = 4;
+            this.NudFontSize.ValueChanged += new System.EventHandler(this.NudFontSize_ValueChanged);
+            // 
+            // FntSize
+            // 
+            this.FntSize.AutoSize = true;
+            this.FntSize.Location = new System.Drawing.Point(26, 67);
+            this.FntSize.Name = "FntSize";
+            this.FntSize.Size = new System.Drawing.Size(51, 14);
+            this.FntSize.TabIndex = 4;
+            this.FntSize.Text = "글자크기";
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(29, 95);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(392, 80);
+            this.TxtResult.TabIndex = 5;
+            this.TxtResult.TextChanged += new System.EventHandler(this.TxtResult_TextChanged);
+            // 
+            // Chkitalic
+            // 
+            this.Chkitalic.AutoSize = true;
+            this.Chkitalic.Location = new System.Drawing.Point(366, 39);
+            this.Chkitalic.Name = "Chkitalic";
+            this.Chkitalic.Size = new System.Drawing.Size(59, 18);
+            this.Chkitalic.TabIndex = 3;
+            this.Chkitalic.Text = "이탤릭";
+            this.Chkitalic.UseVisualStyleBackColor = true;
+            this.Chkitalic.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // ChkBold
+            // 
+            this.ChkBold.AutoSize = true;
+            this.ChkBold.Location = new System.Drawing.Point(300, 39);
+            this.ChkBold.Name = "ChkBold";
+            this.ChkBold.Size = new System.Drawing.Size(48, 18);
+            this.ChkBold.TabIndex = 2;
+            this.ChkBold.Text = "볼드";
+            this.ChkBold.UseVisualStyleBackColor = true;
+            this.ChkBold.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // CboFontFamily
+            // 
+            this.CboFontFamily.BackColor = System.Drawing.SystemColors.Window;
+            this.CboFontFamily.FormattingEnabled = true;
+            this.CboFontFamily.Location = new System.Drawing.Point(86, 34);
+            this.CboFontFamily.Name = "CboFontFamily";
+            this.CboFontFamily.Size = new System.Drawing.Size(193, 22);
+            this.CboFontFamily.TabIndex = 1;
+            this.CboFontFamily.SelectedIndexChanged += new System.EventHandler(this.CboFontFamily_SelectedIndexChanged);
+            // 
+            // Fnt
+            // 
+            this.Fnt.AutoSize = true;
+            this.Fnt.Location = new System.Drawing.Point(26, 39);
+            this.Fnt.Name = "Fnt";
+            this.Fnt.Size = new System.Drawing.Size(40, 14);
+            this.Fnt.TabIndex = 0;
+            this.Fnt.Text = "글자체";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(471, 572);
+            this.Controls.Add(this.GbxMain);
+            this.Font = new System.Drawing.Font("나눔고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "속성확인";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.GbxMain.ResumeLayout(false);
+            this.GbxMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NudFontSize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GbxMain;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.CheckBox ChkBold;
+        private System.Windows.Forms.ComboBox CboFontFamily;
+        private System.Windows.Forms.Label Fnt;
+        private System.Windows.Forms.NumericUpDown NudFontSize;
+        private System.Windows.Forms.Label FntSize;
+        private System.Windows.Forms.CheckBox Chkitalic;
+    }
+}
+
