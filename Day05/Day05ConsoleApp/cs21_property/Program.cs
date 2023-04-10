4f0945a0d5cf92e8d9b12cc42aa5ab3ac95cd7cc
JeongJaeAhn <123913903+JeongJaeAhn@users.noreply.github.com>
2023-04-10 17:20:23 +0900

    23.4.10 월

---
+++ Day05/Day05ConsoleApp/cs21_property/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs21_property
+{
+    class Boiler
+    {
+        public int temp;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boiler kitturami = new Boiler();
+            kitturami.temp = 60;
+
+            // ...
+            kitturami.temp = 300000; // 보일러 물수온이 30만도?
+        }
+    }
+}
